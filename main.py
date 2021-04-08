a34b6ddd9490aeaa420886a837a187099fdaab80
tal1992 <timothylam92@hotmail.co.uk>
2021-04-08 15:44:19 +0100

    Modern Clock

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button,ButtonBehavior
+
+
+class StopwatchScreen(Screen):
+    pass
+
+class ClockScreen(Screen):
+    pass
+
+class MainApp(App):
+    pass
+
+MainApp().run()
