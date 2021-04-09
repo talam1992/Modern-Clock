75b96d6e894a78f46ad155becf49765a39d028fe
tal1992 <timothylam92@hotmail.co.uk>
2021-04-09 13:48:25 +0100

    Adding Widgets to our Screens

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button,ButtonBehavior
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.button import Button, ButtonBehavior
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.image import Image
+from kivy.utils import get_color_from_hex
 
 
-class StopwatchScreen(Screen):
+class ImageButton(ButtonBehavior,Image):
     pass
 
-class ClockScreen(Screen):
+class StopwatchScreen (Screen):
     pass
 
-class MainApp(App):
+
+class ClockScreen (Screen):
     pass
 
-MainApp().run()
+
+class MainApp (App):
+    def go_forward(self):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.transition = SlideTransition(direction="right")
+        screen_manager.current = 'stopwatch_screen'
+
+    def go_back(self):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.transition = SlideTransition (direction="left")
+        screen_manager.current = 'clock_screen'
+
+
+if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex ('#101216')
+    LabelBase.register(name='Roboto', fn_regular='Roboto-Thin.ttf', fn_bold='Roboto-Medium.ttf')
+MainApp ().run ()
