52a6f4220256bb6df73c8b5ef53ee9209c5fd81b
tal1992 <timothylam92@hotmail.co.uk>
2021-04-09 15:54:45 +0100

    Adding Logic to our Application

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from kivy.uix.image import Image
 from kivy.utils import get_color_from_hex
+from kivy.clock import Clock
+from time import strftime
 
+
+Window.borderless = True
 
 class ImageButton(ButtonBehavior,Image):
     pass
@@ -19,6 +23,20 @@ class ClockScreen (Screen):
 
 
 class MainApp (App):
+    sw_started = False
+    sw_seconds = 0
+
+    def update(self,nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+
+        self.root.ids['clock_screen'].ids['time'].text=strftime('[b]%H[/b]:%M:%S')
+        m,s = divmod(self.sw_seconds,60)
+        self.root.ids['stopwatch_screen'].ids['stopwatch'].text=('%02d: %02d.[size=40]%02s[/size]'%(int(m),int(s),int(s*100%100)))
+
+    def on_start(self):
+        Clock.schedule_interval(self.update,0)
+
     def go_forward(self):
         screen_manager = self.root.ids['screen_manager']
         screen_manager.transition = SlideTransition(direction="right")
@@ -28,6 +46,18 @@ class MainApp (App):
         screen_manager = self.root.ids['screen_manager']
         screen_manager.transition = SlideTransition (direction="left")
         screen_manager.current = 'clock_screen'
+
+    def start_stop(self):
+        self.root.ids['stopwatch_screen'].ids['start_stop'].text='Start' if self.sw_started else 'Stop'
+        self.sw_started = not self.sw_started
+        #print("Start Stop")
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids['stopwatch_screen'].ids['start_stop'].text='Start'
+            self.sw_started = False
+        self.sw_seconds = 0
+        #print("Reset")
 
 
 if __name__ == '__main__':
